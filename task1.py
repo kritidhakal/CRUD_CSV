93395d638aa7f5e095b3e91d9fede7fc30b7e646
unknown <dklkrt791@gmail.com>
2022-11-29 12:51:59 +0545

    optimized on storage usage

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 import csv
+import os
 options = input("what do u wanna do signup(s) or login(l) or delete(d) or update(u)?")
 match options:
     case "s":
@@ -19,9 +20,10 @@ match options:
         id = input("Your id please!")
         def checkCred(name, email, id): 
             with open('register.csv', mode='r') as regfile:
+                header = ['Name', 'Email', 'Id']
                 csv_reader = csv.DictReader(regfile, fieldnames = header)
                 for row in csv_reader:
-                    if(row[0]==name and row[1]==email and row[2]==id):
+                    if(row.get("Name")==name and row.get("Email")==email and row.get("Id")==id):
                         print("You are successfully logged in!!")
                         break;
                 else:       
@@ -32,15 +34,11 @@ match options:
         with open('register.csv', 'r') as inp, open('updated.csv', 'w', newline="") as out:
             header = ['Name', 'Email', 'Id']
             writer = csv.DictWriter(out, fieldnames = header)
-            id_val = input("enter the id of that data to be updated: ")
+            id_val = input("enter the id of that data to be deleted: ")
             for row in csv.DictReader(inp, fieldnames = header):
-                if row[2] != id_val:
+                if row.get("Id") != id_val:
                     writer.writerow(row)
-            with open('updated.csv', 'r') as inp, open('register.csv', 'w', newline="") as out:
-                header = ['Name', 'Email', 'Id']
-                writer = csv.DictWriter(out, fieldnames = header)
-                for row in csv.DictReader(inp, fieldnames = header):
-                    writer.writerow(row)
+        os.replace('updated.csv', 'register.csv')       
 
     case "u":
         with open('register.csv', 'r') as inp, open('updated.csv', 'w', newline="") as out:
@@ -55,11 +53,7 @@ match options:
                     writer.writerow({'Name': name, 'Email': email, 'Id': id})
                     continue
                 writer.writerow(row)
-        with open('updated.csv', 'r') as inp, open('register.csv', 'w', newline="") as out:
-            header = ['Name', 'Email', 'Id']
-            writer = csv.DictWriter(out, fieldnames = header)
-            for row in csv.DictReader(inp, fieldnames = header):
-                writer.writerow(row)
+        os.replace('updated.csv', 'register.csv')
                     
 
 
